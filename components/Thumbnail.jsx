ced46cbe4f3b1d99543c3f11adcdfddfe90883b5
NunezAN <alex101nunez@gmail.com>
2022-08-17 14:28:21 -0500

    results is now responsive

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -20,7 +20,7 @@ const Thumbnail = ({ result }) => {
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type}.`}{" "}
-          {result.release_date || result.first_air_date} .{" "}
+          {result.release_date || result.first_air_date} •{" "}
           <ThumbUpIcon className="h-5 mx-2" />
           {result.vote_count}
         </p>
